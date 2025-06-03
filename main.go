6adb2e553ebe2d8ccf025217856d79c63a98af1c
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-06-04 00:17:35 +0300

    chore: animal endpoint

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
-import (
-	"net/http"
+import "github.com/gin-gonic/gin"
 
-	"github.com/gin-gonic/gin"
-)
+type CatForm struct {
+	Key string `json:"catName" binding:"required"`
+}
+
+type DogForm struct {
+	Key string `json:"dogName" binding:"required"`
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-  r.Run()
+
+	router.GET("/asciiJSON", func(ctx *gin.Context) {
+		result := map[string]any{
+			"lang": "Россия",
+		}
+		ctx.AsciiJSON(200, result)
+	})
+
+	router.POST("/animal", func(ctx *gin.Context) {
+		cat := CatForm{}
+		if err := ctx.ShouldBind(&cat); err == nil {
+			ctx.JSON(200, gin.H{
+				"message": "cat over there!",
+			})
+		}
+	})
+
+	router.Run()
 }
