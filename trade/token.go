1af514d1f31282e108dff5ab15cf585e1faf5511
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-06-20 23:28:17 +0300

    chore(token): constructor

--- trade/token.go
+++ trade/token.go
@@ -1,18 +1,50 @@
 package trade
 
 import (
+	"errors"
 	"math/big"
 
+	"github.com/chenzhijie/go-web3"
 	"github.com/chenzhijie/go-web3/eth"
 )
 
 type ERC20 struct {
 	contract eth.Contract
-	decimals uint8
+	decimals big.Int 
 	name     string
 }
 
 func (token *ERC20) Transfer(recipient string, amount big.Int) {
-  token.contract.Call("transfer", recipient, amount)
+	token.contract.Call("transfer", recipient, amount)
 
+}
+
+var (
+	BadDecimalsValue error = errors.New("Bad decimals of ERC20 contract")
+	BadNameValue error = errors.New("Bad decimals of ERC20 contract")
+)
+
+func CreateToken(w3 *web3.Web3, address string) (*ERC20, error) {
+	contract, err := CreateContract(w3, "../abi/ERC20.json", address)
+	if err != nil {
+		return nil, err
+	}
+	decimalsRaw, err := contract.Call("decimals")
+	if err != nil {
+		return nil, err
+	}
+	var decimals *big.Int
+	var success bool
+	if decimals, success = decimalsRaw.(*big.Int); !success {
+		return nil, BadDecimalsValue
+	}
+	nameRaw, err := contract.Call("name")
+	if err != nil {
+		return nil, err
+	}
+	var name *string
+	if name, success = nameRaw.(*string); !success {
+		return nil, BadNameValue
+	}
+	return &ERC20{contract: *contract, decimals: *decimals, name: *name}, nil
 }
