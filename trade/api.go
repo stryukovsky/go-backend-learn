74f0579cf6a6e951c2ca4e8af4b2525e9fe2c346
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-08-31 00:03:17 +0300

    feat(api): endpoints with caching

--- trade/api.go
+++ trade/api.go
@@ -1,17 +1,27 @@
 package trade
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"math/big"
 	"net/http"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
 type BalanceAcrossAllChains struct {
+	Address string `json:"address" binding:"required"`
+	Balance string `json:"balance" binding:"required"`
+}
+
+type BalanceOnChain struct {
+	ChainId string `json:"chainId" binding:"required"`
 	Address string `json:"address" binding:"required"`
 	Balance string `json:"balance" binding:"required"`
 }
@@ -62,14 +72,24 @@ func calculateBalance(income []Deal, outcome []Deal) string {
 	return balance
 }
 
-// TODO: add caching
-func BalanceByWallet(ctx *gin.Context, db *gorm.DB) {
+const cacheKeyPrefixBalanceAcrossAllChains = "balanceAcrossAllChains:"
+
+func BalanceByWallet(ctx *gin.Context, db *gorm.DB, rdb *redis.Client) {
 	walletAddress := common.HexToAddress(ctx.Param("wallet")).Hex()
+	cacheKey := cacheKeyPrefixBalanceAcrossAllChains + walletAddress
+	cachedBalance, err := rdb.Get(context.Background(), cacheKey).Result()
+	if err == nil && cachedBalance != "" {
+		var balanceAcrossAllChains BalanceAcrossAllChains
+		json.Unmarshal([]byte(cachedBalance), &balanceAcrossAllChains)
+		ctx.JSON(http.StatusOK, balanceAcrossAllChains)
+		return
+	}
+
 	slog.Info(fmt.Sprintf("Find balances across all blockchains of %s", walletAddress))
 
 	var dealsIncome []Deal
 	countIncome := 0
-	err := db.Preload("BlockchainTransfer").Where("blockchain_transfer.recipient = ?", walletAddress).First(&dealsIncome, &countIncome).Error
+	err = db.Preload("BlockchainTransfer").Where("blockchain_transfer.recipient = ?", walletAddress).First(&dealsIncome, &countIncome).Error
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Cannot fetch income deals for %s: %s", walletAddress, err.Error)})
 		return
@@ -84,27 +104,71 @@ func BalanceByWallet(ctx *gin.Context, db *gorm.DB) {
 	slog.Info(fmt.Sprintf("Found %d income and %d outcome deals of %s", len(dealsIncome), countOutcome, walletAddress))
 
 	balance := calculateBalance(dealsIncome, dealsOutcome)
+	cachedData, _ := json.Marshal(BalanceAcrossAllChains{Address: walletAddress, Balance: balance})
+	rdb.Set(ctx, cacheKey, cachedData, 5*time.Minute)
 	ctx.JSON(http.StatusOK, BalanceAcrossAllChains{Address: walletAddress, Balance: balance})
 }
 
-// TODO: add caching
-func BalanceByWalletAndChain(ctx *gin.Context, db *gorm.DB) {
+const cacheKeyPrefixBalanceOfWalletOnChain = "balanceOnChain:"
+
+func BalanceByWalletAndChain(ctx *gin.Context, db *gorm.DB, rdb *redis.Client) {
 	walletAddress := common.HexToAddress(ctx.Param("wallet")).Hex()
 	chainId := ctx.Param("chainId")
+
+	key := cacheKeyPrefixBalanceOfWalletOnChain + chainId + ":" + walletAddress
+	cached, err := rdb.Get(context.Background(), key).Result()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "could not get balance from cache due to internal error " + err.Error()})
+		return
+	}
+
+	if cached != "" {
+		var result BalanceOnChain
+		deserialized := json.Unmarshal([]byte(cached), &result)
+		ctx.JSON(http.StatusOK, deserialized)
+		return
+	}
+
 	dealsIncome := []Deal{}
 	dealsOutcome := []Deal{}
-	err := db.Preload("BlockchainTransfer").Find(&dealsIncome, Deal{BlockchainTransfer: ERC20Transfer{Recipient: walletAddress, ChainId: chainId}}).Error
+	err = db.
+		Preload("BlockchainTransfer").
+		Find(&dealsIncome, Deal{BlockchainTransfer: ERC20Transfer{Recipient: walletAddress, ChainId: chainId}}).
+		Error
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": fmt.Sprintf("Cannot fetch income deals for %s: %s", walletAddress, err.Error),
 		})
 		return
 	}
-	err = db.Preload("BlockchainTransfer").Find(&dealsOutcome, Deal{BlockchainTransfer: ERC20Transfer{Sender: walletAddress, ChainId: chainId}}).Error
+	err = db.
+		Preload("BlockchainTransfer").
+		Find(&dealsOutcome, Deal{BlockchainTransfer: ERC20Transfer{Sender: walletAddress, ChainId: chainId}}).
+		Error
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Cannot fetch income deals for %s: %s", walletAddress, err.Error())})
 		return
 	}
+
+	balance := big.NewRat(0, 1)
+	for _, deal := range dealsIncome {
+		balance = balance.Add(balance, deal.VolumeUSD.Rat)
+	}
+	for _, deal := range dealsOutcome {
+		balance = balance.Sub(balance, deal.VolumeUSD.Rat)
+	}
+	result := BalanceOnChain{
+		ChainId: chainId,
+		Address: walletAddress,
+		Balance: balance.FloatString(2),
+	}
+	err = rdb.Set(context.Background(), key, result, 15*time.Minute).Err()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Cannot set to cache balance", err.Error())})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
 }
 
 func GetWalletsOnChain(ctx *gin.Context, db *gorm.DB) {
@@ -119,7 +183,7 @@ func GetWalletsOnChain(ctx *gin.Context, db *gorm.DB) {
 
 }
 
-func CreateApi(router *gin.Engine, db *gorm.DB) {
+func CreateApi(router *gin.Engine, db *gorm.DB, rdb *redis.Client) {
 	router.POST("/api/deal", func(ctx *gin.Context) {
 		AddDeal(ctx, db)
 	})
@@ -133,6 +197,9 @@ func CreateApi(router *gin.Engine, db *gorm.DB) {
 		GetWalletsOnChain(ctx, db)
 	})
 	router.GET("/api/balance/:chainId/:wallet", func(ctx *gin.Context) {
-		BalanceByWalletAndChain(ctx, db)
+		BalanceByWalletAndChain(ctx, db, rdb)
+	})
+	router.GET("/api/balance/:wallet", func(ctx *gin.Context) {
+		BalanceByWallet(ctx, db, rdb)
 	})
 }
