b3d7f591896125ee6cb7c19a2fb1ea975e98c532
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-12-30 22:54:43 +0300

    chore(fixture): add more chains and wallets in separate file

--- startdb.sh
+++ startdb.sh
@@ -1,4 +1,4 @@
-docker run -d --name postgresql_database -e POSTGRES_USER=user -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=db -p 5432:5432 postgres 2>/dev/null
+docker run -d --name postgresql_database_my_wallets -e POSTGRES_USER=user -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=db -p 5432:5432 postgres 2>/dev/null
 sleep 3
 docker restart postgresql_database 2>/dev/null
 
--- trade/database/fixture.go
+++ trade/database/fixture.go
@@ -1,24 +1,204 @@
 package database
 
 import (
+	"encoding/json"
 	"math/big"
+	"os"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/lib/pq"
+	"github.com/samber/lo"
 	"github.com/stryukovsky/go-backend-learn/trade"
 	"gorm.io/gorm"
 )
 
-func Fixture(db *gorm.DB) {
+func Arbitrum(db *gorm.DB) {
+	// Arbitrum Chain ID is 42161
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+			Symbol:   "USDT",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+			Symbol:   "WETH",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	// Note: No direct EUR stablecoin equivalent found on Arbitrum
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+			Symbol:   "USDC",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	// Note: BNB is not natively available on Arbitrum (it's a BSC token)
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+			Symbol:   "LINK",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+			Symbol:   "UNI",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+			Symbol:   "WBTC",
+			Decimals: trade.NewDBInt(big.NewInt(8)),
+		})
+	db.Create(&trade.Chain{Name: "Arbitrum One", ChainId: "42161"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Aave, ChainId: "42161", Address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Compound3, ChainId: "42161", Address: "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf"})
+}
+
+func Binance(db *gorm.DB) {
+	// BSC Chain ID is 56
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0x55d398326f99059fF775485246999027B3197955",
+			Symbol:   "USDT",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+			Symbol:   "BNB",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+			Symbol:   "BUSD",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+			Symbol:   "USDC",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+			Symbol:   "ETH",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0xF8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD",
+			Symbol:   "LINK",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0xBf5140A22578168FD562DCcF235E5D43A02ce9B1",
+			Symbol:   "UNI",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "56",
+			Address:  "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+			Symbol:   "BTC",
+			Decimals: trade.NewDBInt(big.NewInt(8)),
+		})
+	db.Create(&trade.Chain{Name: "BNB Smart Chain", ChainId: "56"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Aave, ChainId: "56", Address: "0x6807dc923806fE8Fd134338EABCA509979a7e0cB"})
+}
+
+func Base(db *gorm.DB) {
+	// Base Chain ID is 8453
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+			Symbol:   "USDT",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0x4200000000000000000000000000000000000006",
+			Symbol:   "ETH",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+			Symbol:   "USDC",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0x1d4bB5576050E69a95d8A726dF4fF23125130EB5",
+			Symbol:   "LINK",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0x90dF94286a30E52a776ed76Ec97d9dAC6C745936",
+			Symbol:   "UNI",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "8453",
+			Address:  "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c",
+			Symbol:   "BTC",
+			Decimals: trade.NewDBInt(big.NewInt(8)),
+		})
+	db.Create(&trade.Chain{Name: "Base", ChainId: "8453"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Aave, ChainId: "8453", Address: "0xA4CF4740E5F564D2BFC7382352c61F202D894287"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Compound3, ChainId: "8453", Address: "0xBA12222222228d8Ba445958a75a0704d566BF2C8"})
+}
+
+func Wallets(db *gorm.DB) {
+	addresses := []string{}
+	addressesSource, err := os.ReadFile("wallets.json")
+	if err != nil {
+		panic("Cannot read wallets file")
+	}
+	err = json.Unmarshal(addressesSource, &addresses)
+	if err != nil {
+		panic("Cannot read wallets file: file exists but malformed")
+	}
+	lo.ForEach(addresses, func(item string, index int) {
+		db.Create(&trade.TrackedWallet{ChainId: "42161", Address: common.HexToAddress(item).String(), LastBlock: 210147506})
+		db.Create(&trade.TrackedWallet{ChainId: "56", Address: item, LastBlock: 33484103})
+		db.Create(&trade.TrackedWallet{ChainId: "8453", Address: item, LastBlock: 20163035})
+	})
+}
+
+func Ethereum(db *gorm.DB) {
 	url := "https://eth-mainnet.public.blastapi.io"
 	blockchainUrls := make(pq.StringArray, 1)
 	blockchainUrls[0] = url
 
 	db.Create(&trade.Worker{BlockchainUrls: blockchainUrls, BlocksInterval: 1000})
 	db.Create(&trade.AnalyticsWorker{BlockchainUrls: blockchainUrls, BlocksInterval: 1000, LastBlock: 12369651})
-	db.Create(&trade.TrackedWallet{ChainId: "1", Address: "0x8EB8a3b98659Cce290402893d0123abb75E3ab28", LastBlock: 23152597})
-	db.Create(&trade.TrackedWallet{ChainId: "1", Address: "0x49ecd0F2De4868E5130fdC2C45D4d97444B7c269", LastBlock: 23495633})
-	db.Create(&trade.TrackedWallet{ChainId: "1", Address: "0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af", LastBlock: 23153501})
-	db.Create(&trade.TrackedWallet{ChainId: "1", Address: "0xa1EF1AF5934b2b9771075cC14fF38368Da630409", LastBlock: 24105400})
+
 	db.Create(
 		&trade.Token{
 			ChainId:  "1",
