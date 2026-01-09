35d9c40517baf4253c03d586828bfde7a3db8751
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2026-01-09 21:20:45 +0300

    chore(misc): uncomment

--- trade/database/fixture.go
+++ trade/database/fixture.go
@@ -38,54 +38,54 @@ func Arbitrum(db *gorm.DB) {
 	blockchainUrlsForCache = append(blockchainUrlsForCache, "https://arb-one.api.pocket.network")
 
 	db.Create(&trade.Worker{BlockchainUrlsForEvents: blockchainUrlsForEvents, BlockchainUrlsForCacheManager: blockchainUrlsForCache, BlocksInterval: 50000})
-	// Arbitrum Chain ID is 42161
-	// db.Create(
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
-	// 		Symbol:   "USDT",
-	// 		Decimals: trade.NewDBInt(big.NewInt(6)),
-	// 	})
-	// db.Create(
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-	// 		Symbol:   "WETH",
-	// 		Decimals: trade.NewDBInt(big.NewInt(18)),
-	// 	})
-	// db.Create(
-	// 	// Note: No direct EUR stablecoin equivalent found on Arbitrum
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-	// 		Symbol:   "USDC",
-	// 		Decimals: trade.NewDBInt(big.NewInt(6)),
-	// 	})
-	// // Note: BNB is not natively available on Arbitrum (it's a BSC token)
-	// db.Create(
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
-	// 		Symbol:   "LINK",
-	// 		Decimals: trade.NewDBInt(big.NewInt(18)),
-	// 	})
-	// db.Create(
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
-	// 		Symbol:   "UNI",
-	// 		Decimals: trade.NewDBInt(big.NewInt(18)),
-	// 	})
-	// db.Create(
-	// 	&trade.Token{
-	// 		ChainId:  "42161",
-	// 		Address:  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
-	// 		Symbol:   "WBTC",
-	// 		Decimals: trade.NewDBInt(big.NewInt(8)),
-	// 	})
-	// db.Create(&trade.Chain{Name: "Arbitrum One", ChainId: "42161"})
-	// db.Create(&trade.DeFiPlatform{Type: trade.Aave, ChainId: "42161", Address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD"})
-	// db.Create(&trade.DeFiPlatform{Type: trade.Compound3, ChainId: "42161", Address: "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf"})
+	Arbitrum Chain ID is 42161
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+			Symbol:   "USDT",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+			Symbol:   "WETH",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		// Note: No direct EUR stablecoin equivalent found on Arbitrum
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+			Symbol:   "USDC",
+			Decimals: trade.NewDBInt(big.NewInt(6)),
+		})
+	// Note: BNB is not natively available on Arbitrum (it's a BSC token)
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+			Symbol:   "LINK",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+			Symbol:   "UNI",
+			Decimals: trade.NewDBInt(big.NewInt(18)),
+		})
+	db.Create(
+		&trade.Token{
+			ChainId:  "42161",
+			Address:  "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+			Symbol:   "WBTC",
+			Decimals: trade.NewDBInt(big.NewInt(8)),
+		})
+	db.Create(&trade.Chain{Name: "Arbitrum One", ChainId: "42161"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Aave, ChainId: "42161", Address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD"})
+	db.Create(&trade.DeFiPlatform{Type: trade.Compound3, ChainId: "42161", Address: "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf"})
 }
 
 func Binance(db *gorm.DB) {
